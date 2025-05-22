c6fb2eb184f8755fdf7564f40cb8791aadc7c8c1
Maxhhh123 <1109230489@qq.com>
2025-05-22 16:54:44 +0800

    修改了mapper的传输sql语句

--- src/main/java/com/karry/springbootmybatis/mapper/HumanRMapper.java
+++ src/main/java/com/karry/springbootmybatis/mapper/HumanRMapper.java
@@ -14,19 +14,24 @@ public interface HumanRMapper {
     @Select("SELECT * FROM \"human_res\" WHERE year = #{year} AND stage = #{stage} AND location = #{location}")
     List<Map<String, Object>> getFilteredData(@Param("year") Integer year, @Param("stage") String stage, @Param("location") String location);
 
-    @Select("SELECT \"EduCost\" FROM \"financial_res\" WHERE year = #{year}")
-    List<Map<String, Object>> getEduCost(@Param("year") Integer year);
+//    @Select("SELECT \"EduCost\" FROM \"financial_res\" WHERE year = #{year}")
+//    List<Map<String, Object>> getEduCost(@Param("year") Integer year);
 
+    @Select("SELECT \"EduCost\" FROM \"financial_res\" WHERE year = #{year} AND location= '全国' ")
+    List<Map<String, Object>> getEduCost(@Param("year") Integer year);
     @Select("SELECT \"gdp\" FROM \"financial_res\" WHERE year = #{year} AND location = '全国'")//这里要指出，这里的location是一个字符串，所以要用单引号括起来，全国是location
     List<Map<String, Object>> getNationalGDP(@Param("year") Integer year);
 
-    @Select("SELECT \"CulCost\" FROM \"financial_res\" WHERE year = #{year}")//这里的year是一个数字，所以不需要单引号
+//    @Select("SELECT \"CulCost\" FROM \"financial_res\" WHERE year = #{year}")//这里的year是一个数字，所以不需要单引号
+//    List<Map<String, Object>> getCulCost(@Param("year") Integer year);
+
+    @Select("SELECT \"CulCost\" FROM \"financial_res\" WHERE year = #{year} AND location= '全国'")//这里的year是一个数字，所以不需要单引号
     List<Map<String, Object>> getCulCost(@Param("year") Integer year);
 
-    @Select("SELECT \"area\" FROM \"material resources\" WHERE year = #{year} AND location = '全国'")//这里的year是一个数字，所以不需要单引号
+    @Select("SELECT \"area\" FROM \"material resources\" WHERE year = #{year} AND location='全国'and stage IS NULL")//这里的year是一个数字，所以不需要单引号
     List<Map<String, Object>> getArea(@Param("year") Integer year);
 
-    @Select("SELECT \"fixedassets\" FROM \"material resources\" WHERE year = #{year} AND location = '全国'")
+    @Select("SELECT \"fixedassets\" FROM \"material resources\" WHERE year = #{year} AND location = '全国'and stage IS NULL")
     List<Map<String, Object>> getfixedassets(@Param("year") Integer year);
 
     @Select("SELECT \"Snum\" FROM \"human_res\" WHERE stage='小学' AND location=#{province} AND year IN (2018, 2019, 2020, 2021, 2022) ORDER BY year ASC")
